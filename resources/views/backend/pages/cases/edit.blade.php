92e493d73e66456c4bc78f3c62c1c03b9c68d46e
ashwiniweb <63944221+ashwiniweb@users.noreply.github.com>
2024-08-20 15:34:26 +0530

    edit case

--- resources/views/backend/pages/cases/edit.blade.php
+++ resources/views/backend/pages/cases/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.layouts.master')
 
 @section('title')
-Fi Type Edit - Admin Panel
+Case Edit - Admin Panel
 @endsection
 
 @section('styles')
@@ -25,8 +25,8 @@ Fi Type Edit - Admin Panel
                 <h4 class="page-title pull-left">Fi Type Create</h4>
                 <ul class="breadcrumbs pull-left">
                     <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                    <li><a href="{{ route('admin.fitypes.index') }}">All Fi Types</a></li>
-                    <li><span>Edit Fi Type - {{ $fitype->name }}</span></li>
+                    <li><a href="{{ route('admin.fitypes.index') }}">All Create Case</a></li>
+                    <li><span>Edit Case</span></li>
                 </ul>
             </div>
         </div>
@@ -43,23 +43,109 @@ Fi Type Edit - Admin Panel
         <div class="col-12 mt-5">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="header-title">Edit Fi Type - {{ $fitype->name }}</h4>
+                    <h4 class="header-title">Edit Case </h4>
                     @include('backend.layouts.partials.messages')
 
-                    <form action="{{ route('admin.fitypes.update', $fitype->id) }}" method="POST">
-                        @method('PUT')
+                    <form action="{{ route('admin.cases.update',$cases->id) }}" method="POST">
                         @csrf
+                        @method('PUT')
                         <div class="form-row">
                             <div class="form-group col-md-6 col-sm-12">
-                                <label for="name">Fi Type Name</label>
-                                <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="{{ $fitype->name }}">
+                                <label for="name">Bank</label>
+                                <select class="custom-select selectBank" name="bank_id" id="selectBank">
+                                    <option value="">--Select Option--</option>
+                                    @foreach ($banks as $bank)
+                                        <option value="{{ $bank['id'] }}" @if($cases->bank_id == $bank['id']) selected @endif  >{{ $bank['name'] }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group col-md-6 col-sm-12">
+                                <label for="name">Product</label>
+                                <select id="productSelect" name="product_id" class="custom-select">
+                                    <option value="">--Select Option--</option>
+                                    @if($AvailbleProduct)
+                                        @foreach ($AvailbleProduct as $product)
+                                            <option value="{{ $product['id'] }}" @if($cases->product_id == $product['id']) selected @endif  >{{ $product['name'] }}</option>
+                                        @endforeach
+                                    @endif
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6 col-sm-12">
+                                <label for="name">FI Type</label>
+                                @foreach ($fitypes as $fitype)
+                                    <div class="form-check">
+                                        <input class="form-check-input fytpe_checkbox" type="checkbox" name="fi_type_id[][id]" value="{{ $fitype['id'] }}" rel-name="{{ $fitype['name'] }}"   @if(isset($fi_type_ids) && in_array($fitype['id'],$fi_type_ids) )  checked   @endif>
+                                        <label class="form-check-label" for="fitype{{ $fitype['id'] }}"> {{ $fitype['name'] }} </label>
+                                    </div>
+                                @endforeach
+                            </div>
+                            {!! $fitypesFeild !!}
+                            <div class="form-group col-md-6 col-sm-12">
+                                <label for="application_type">Application Type</label>
+                                <select class="custom-select application_type" name="application_type" id="application_type">
+                                    <option value="">--Select Option--</option>
+                                    @foreach ($ApplicationTypes as $ApplicationType)
+                                        <option value="{{ $ApplicationType['id'] }}" @if($cases->application_type == $ApplicationType['id']) selected @endif>{{ $ApplicationType['name'] }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-6 col-sm-12">
+                                <label for="name">Reference Number</label>
+                                <input type="text" class="form-control" id="refrence_number" name="refrence_number" placeholder="Enter Reference Number" value="{{ $cases->refrence_number ?? '' }}">
+                            </div>
+                            <div class="form-group col-md-6 col-sm-12">
+                                <label for="name">Amount</label>
+                                <input type="number" class="form-control" id="amount" name="amount" placeholder="Enter Amount" value="{{ $cases->amount ?? '' }}">
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-6 col-sm-12 name Applicant co_applicant_name d-none">
+                                <label for="name">Applicant Name</label>
+                                <input type="text" class="form-control" name="applicant_name" placeholder="Enter Applicant Name" value="{{ $cases->applicant_name ?? '' }}">
+                            </div>
+                            <div class="form-group col-md-6 col-sm-12 name co_applicant_name d-none">
+                                <label for="name">Co-Applicant Name</label>
+                                <input type="text" class="form-control" name="co_applicant_name" placeholder="Enter Co-Applicant Name" value="{{ $cases->co_applicant_name ?? '' }}">
+                            </div>
+                            <div class="form-group col-md-6 col-sm-12 name Guranter d-none">
+                                <label for="name">Guarantee Name</label>
+                                <input type="text" class="form-control" name="guarantee_name" placeholder="Enter Guarantee Name" value="{{ $cases->guarantee_name ?? '' }}">
+                            </div>
+
+                            <div class="form-group col-md-6 col-sm-12 name Seller d-none">
+                                <label for="name">Seller Name</label>
+                                <input type="text" class="form-control" name="seller_name" placeholder="Enter Seller Name" value="{{ $cases->applicant_name ?? '' }}">
+                            </div>
+                        </div>
+                        <div class="form-row">
+
+                            <div class="form-group col-md-6 col-sm-12">
+                                <label for="name">Vehicle</label>
+                                <input type="text" class="form-control" id="vehicle" name="vehicle" placeholder="Enter Vehicle" value="{{ $cases->vehicle ?? '' }}">
+                            </div>
+                            <div class="form-group col-md-6 col-sm-12">
+                                <label for="geo_limit">Geo Limit *</label>
+                                <select id="geo_limit" name="geo_limit" class="custom-select">
+                                    <option value="">--Select Option--</option>
+                                    <option value="Local" @if($cases->geo_limit == 'Local') selected @endif>Local</option>
+                                    <option value="Outstation" @if($cases->geo_limit == 'Outstation') selected @endif>Outstation</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6 col-sm-12">
+                                <label for="name">Remarks</label>
+                                <textarea name="remarks" rows="2" cols="20" id="remarks" class="form-control" placeholder="Remarks">{{ $cases->remarks ?? '' }}</textarea>
                             </div>
 
                         </div>
-
-
-
-                        <button type="submit" class="btn btn-primary mt-4 pr-4 pl-4">Save Fi Type</button>
+                        <button type="submit" class="btn btn-primary mt-4 pr-4 pl-4">Update Case</button>
                     </form>
                 </div>
             </div>
@@ -76,5 +162,80 @@ Fi Type Edit - Admin Panel
     $(document).ready(function() {
         $('.select2').select2();
     })
+</script>
+<script>
+
+    function appliCationType(selectedApplicationType) {
+        $('.name').addClass('d-none');
+        if (selectedApplicationType == 'Applicant/Co-Applicant') {
+            $(".co_applicant_name").removeClass('d-none');
+        } else {
+            $('.' + selectedApplicationType).removeClass('d-none');
+        }
+        return  true;
+    }
+
+    $(document).ready(function() {
+        var baseUrl = "{{ url('/') }}";
+
+        let selectedApplicationType = $('#application_type').find("option:selected").text();
+        appliCationType(selectedApplicationType);
+
+        $('.fytpe_checkbox').each(function () {
+            var isChecked = $(this).prop('checked');
+            var relName = $(this).attr('rel-name');
+            if (isChecked) {
+                console.log();
+                $('.' + relName + "_section").removeClass('d-none');
+            } else {
+                $('.' + relName + "_section").addClass('d-none');
+            }
+        });
+
+        $('#selectBank').on('change', function(e) {
+            var bankId = $(this).val();
+            var customGetPath = "{{ route('admin.case.item','ID')}}";
+            customGetPath = customGetPath.replace('ID', bankId);
+            $.ajax({
+                url: customGetPath,
+                type: 'GET',
+                success: function(response) {
+                    var select = $('#productSelect');
+                    select.empty(); // Clear any existing options
+                    select.append('<option value="">--Select Option--</option>'); // Add default option
+                    $.each(response, function(key, products) {
+                        $.each(products, function(index, product) {
+                            console.log(product);
+                            var option = $('<option></option>')
+                                .attr('value', product.product_id)
+                                .text(product.name + ' (' + product.product_code + ')');
+                            select.append(option);
+                        });
+                    });
+                },
+                error: function() {
+                    alert('Request failed');
+                }
+            });
+        });
+        $('#application_type').on('change', function(e) {
+            var selectedApplicationType = $(this).find("option:selected").text();
+            appliCationType(selectedApplicationType);
+
+        });
+
+
+        $(document).on('click', '.fytpe_checkbox', function() {
+            var isChecked = $(this).prop('checked');
+            var relName = $(this).attr('rel-name');
+            if (isChecked) {
+                console.log();
+                $('.' + relName + "_section").removeClass('d-none');
+            } else {
+                $('.' + relName + "_section").addClass('d-none');
+            }
+        });
+
+    });
 </script>
 @endsection
